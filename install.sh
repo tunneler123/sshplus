b268f30a8fad51599aaf02b177cd41a053462fa5
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2020-08-26 15:03:51 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+##Download script
+wget https://raw.githubusercontent.com/tunneler123/sshplus/master/script.py
+##Screen
+apt-get install screen
+##python install
+apt-get install python
+###Work
+screen -s python ./script.py
+
+###TUNNELER###
