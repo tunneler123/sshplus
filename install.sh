c15433dfa01dec039ad13a9989f6af1bf7763535
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2021-08-01 02:09:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 ##Download script
 wget https://raw.githubusercontent.com/tunneler123/sshplus/master/script.py
 ##Screen
-apt-get install screen
+apt-get install screen -y
 ##python install
-apt-get install python
+apt-get install python -y
 ###Work
 chmod a+x script.py
 screen -dmS screen python ./script.py
 ###TUNNELER###
 clear
 echo -e "\e[1;32m PYTHON PROXY INSTALLED \e[0m"
-echo -e "\e[1;32m DEFAULT PORT IS 1 \e[0m"
+echo -e "\e[1;32m DEFAULT PORT IS 80 \e[0m"
