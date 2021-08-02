ec2b15959a085db364e369fd30b6d7c0d86451b3
tunneler123 <55013273+tunneler123@users.noreply.github.com>
2021-08-02 14:33:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,11 +10,20 @@ apt-get install python -y
 ###Work
 chmod a+x script.py
 screen -dmS screen python ./script.py
+cat <<EOF >>/usr/bin/start
+screen -dmS screen python /root/script.py
+EOF
+cat <<EOF >>/usr/bin/stop
+pkill python
+EOF
+chmod +x start
+chmod +x stop
 cd /usr/bin
 wget -O account https://raw.githubusercontent.com/tunneler123/sshplus/master/account.sh
 chmod +x account
 ###TUNNELER###
 clear
+echo -e "\e[1;32m PHTUNNELER AUTOSCRIPT \e[0m"
 echo -e "\e[1;32m SSH INSTALLED DONE \e[0m"
 echo -e "\e[1;32m DEFAULT PORT IS 80 \e[0m"
 echo -e "\e[1;32m type "account" to add user \e[0m"
